660794ccaccb45cff44a5ea909fd277b10f1d8fe
Anthonylin2066 <aylin1@ualr.edu>
2023-01-26 20:40:03 +0000

    Updated Bruce Circle Assignment

--- circleassignment-debugging/assets/js/triangle.js
+++ circleassignment-debugging/assets/js/triangle.js
@@ -4,16 +4,19 @@ $( "#CircleForm" ).validate({
 
 });
 
-function displayDiameter() {
+function displayCircle() {
     // if the form is valid, then make the calculations
     if ($("#CircleForm").valid()) {
         
         document.getElementById("diameter").innerHTML = "";
+        document.getElementById("circumference").innerHTML = "";
+        document.getElementById("area").innerHTML = "";
 
          var radius; // string representation of the radius
          var radiusfp; // floating point value of radius
-         var x = 2;
          var diameter;  // floating point Diameter
+         var circumference;  // floating point Diameter
+         var area;  // floating point Diameter
          var result; // displayable result
 
          // read in the radius as a string
@@ -23,23 +26,44 @@ function displayDiameter() {
          radiusfp = parseFloat( radius ); 
 
          // calculate the diameter
-         diameter = calcDiameter(radiusfp, x);
+         diameter = calcDiameter(radiusfp);
+         circumference = calcCircumference(radiusfp);
+         area = calcArea(radiusfp);
 
          // display the diameter
          document.getElementById("diameter").innerHTML = diameter.toString();
+         document.getElementById("circumference").innerHTML = circumference.toString();
+         document.getElementById("area").innerHTML = area.toString();
     }
 }
 
-  function calcDiameter (radiusvalue, x)
+  function calcDiameter (r)
   // returns diameter of a circle
   {
-      return radiusvalue * x;
+      return 2 * r;
+  }
+
+  function calcCircumference (r)
+  // returns circumference of a circle
+  {
+      return 2 * Math.PI * r;
+  }
+
+  function calcArea (r)
+  // returns area of a circle
+  {
+      return Math.PI * r * r;
   }
 
   function clearForm()
 {
     document.getElementById("radius").value = "";
     document.getElementById("radiuserror").innerHTML = "";
+    document.getElementById("diameter").value = "";
     document.getElementById("diameter").innerHTML = "";
-    document.getElementById("circumfrence").innerHTML = "";
+    document.getElementById("circumference").innerHTML = "";
+    document.getElementById("circumference").value = "";
+    document.getElementById("area").innerHTML = "";
+    document.getElementById("area").value = "";
+
 }

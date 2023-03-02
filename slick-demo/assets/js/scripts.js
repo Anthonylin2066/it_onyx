79fc36099c5b6fe93babe8d3bb5906fa60c0a513
Anthonylin2066 <aylin1@ualr.edu>
2023-03-02 19:07:46 +0000

    added slick demo

---
+++ slick-demo/assets/js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+    $('.fade').slick({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      slide: 'div',
+      cssEase: 'linear',
+      autoplay: true,
+      autoplaySpeed: 2000
+    });
+
+
+});
